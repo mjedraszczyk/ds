e3bdfea48a1eed6b5af1063314acb3b8633159ff
Marcin Jędraszczyk <marcin.jedraszczyk@gmail.com>
2020-11-25 18:39:53 +0100

    Fixed algorithm.

--- src/main/java/SubstringFinder.java
+++ src/main/java/SubstringFinder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubstringFinder {
 
     private static final char SPECIAL_SEARCH_CHAR = 8; // backspace char
@@ -7,54 +10,61 @@ public class SubstringFinder {
             System.err.println("You must provide exactly 2 parameters");
         } else {
             SubstringFinder finder = new SubstringFinder();
-            int index = finder.find(args[0], args[1]);
-            System.out.println("Text \"" + args[0] + "\" " + (index == -1 ? "does not contain" : "contains") + " substring \"" + args[1] + "\"");
+            boolean isSubstring = finder.isSubstring(args[0], args[1]);
+            System.out.println("Text \"" + args[0] + "\" " + (isSubstring ? "does not contain" : "contains") + " substring \"" + args[1] + "\"");
         }
     }
 
-    public int find(String text, String search) {
-        char[] textChars = text.toCharArray();
-        char[] searchPhraseChars = normalizeSearchPhrase(search).toCharArray();
-        int max = textChars.length;
+    public boolean isSubstring(String text, String searchPhrase) {
+        if (searchPhrase.length() == 0) {
+            return true;
+        }
+        String normalizeSearchPhrase = normalizeSearchPhrase(searchPhrase);
+        if (normalizeSearchPhrase.length() == 1 && normalizeSearchPhrase.charAt(0) == SPECIAL_SEARCH_CHAR) {
+            return true;
+        }
+        List<String> searchPhrases = toSearchPhrases(searchPhrase);
+        return find(text.toCharArray(), searchPhrases, 0, 0);
+    }
+
+    private boolean find(char[] text, List<String> searchPhrases, int from, int searchPhraseIdx) {
+        char[] searchPhraseChars = searchPhrases.get(searchPhraseIdx).toCharArray();
         char first = searchPhraseChars[0];
-        for (int i = 0; i < max; i++) {
-            if (!compareChars(textChars[i], first)) {
+        for (int i = from; i < text.length; i++) {
+            if (!compareChars(text[i], first)) {
                 do {
                     i++;
-                } while (i < max && !compareChars(textChars[i], first));
+                } while (i < text.length && !compareChars(text[i], first));
             }
 
-            if (i < max) {
+            if (i < text.length) {
                 int j = i;
                 int k = 0;
 
                 for (; k < searchPhraseChars.length; ++k) {
-                    if (searchPhraseChars[k] == SPECIAL_SEARCH_CHAR && k + 1 < searchPhraseChars.length) {
-                        k++;
-                        while (j < textChars.length && !compareChars(textChars[j], searchPhraseChars[k])) {
-                            j++;
-                        }
-                    }
-                    if (j >= textChars.length || !compareChars(textChars[j], searchPhraseChars[k])) {
+                    if (j >= text.length || !compareChars(text[j], searchPhraseChars[k])) {
                         break;
                     }
                     ++j;
                 }
 
                 if (k == searchPhraseChars.length) {
-                    return i;
+                    if (searchPhraseIdx + 1 < searchPhrases.size()) {
+                        return find(text, searchPhrases, j, ++searchPhraseIdx);
+                    } else {
+                        return true;
+                    }
                 }
             }
         }
-        return -1;
+        return false;
     }
 
     /**
      * Normalize given searchPhrase. First method replaces:
      * - sequence of chars '\' and '*' by '*',
      * - char '*' by SPECIAL_SEARCH_CHAR.
-     * Then it replace multiple occurrences of SPECIAL_SEARCH_CHAR in a row, by one SPECIAL_SEARCH_CHAR. And at the end
-     * it removes trailing SPECIAL_SEARCH_CHARs.
+     * Then it replace multiple occurrences of SPECIAL_SEARCH_CHAR in a row, by one SPECIAL_SEARCH_CHAR.
      *
      * @param searchPhrase
      * @return
@@ -64,7 +74,7 @@ public class SubstringFinder {
         char previousChar = '1';
         for (int i = 0; i < searchPhrase.length(); i++) {
             char currentChar = searchPhrase.charAt(i);
-            if (searchPhrase.charAt(i) == '\\' && searchPhrase.charAt(i + 1) == '*') {
+            if (searchPhrase.charAt(i) == '\\' && i + 1 < searchPhrase.length() && searchPhrase.charAt(i + 1) == '*') {
                 currentChar = '*';
                 i++;
             } else if (searchPhrase.charAt(i) == '*') {
@@ -75,15 +85,30 @@ public class SubstringFinder {
             }
             previousChar = currentChar;
         }
-        if (builder.length() > 1) {
-            if (builder.charAt(0) == SPECIAL_SEARCH_CHAR) {
-                builder.deleteCharAt(0);
-            }
-            if (builder.charAt(builder.length() - 1) == SPECIAL_SEARCH_CHAR) {
-                builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
+    }
+
+    private static List<String> toSearchPhrases(String searchPhrase) {
+        List<String> result = new ArrayList<>(searchPhrase.length());
+        StringBuilder builder = new StringBuilder(searchPhrase.length());
+        for (int i = 0; i < searchPhrase.length(); i++) {
+            char currentChar = searchPhrase.charAt(i);
+            if (searchPhrase.charAt(i) == '\\' && searchPhrase.length() > i + 1 && searchPhrase.charAt(i + 1) == '*') {
+                builder.append('*');
+                i++;
+            } else if (searchPhrase.charAt(i) == '*') {
+                if (builder.length() > 0) {
+                    result.add(builder.toString());
+                    builder = new StringBuilder();
+                }
+            } else {
+                builder.append(currentChar);
             }
         }
-        return builder.toString();
+        if (builder.length() > 0) {
+            result.add(builder.toString());
+        }
+        return result;
     }
 
     private boolean compareChars(char original, char pattern) {
--- src/test/java/SubstringFinderTest.java
+++ src/test/java/SubstringFinderTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -6,27 +7,51 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Test SubstringFinder")
 class SubstringFinderTest {
 
     private SubstringFinder substringFinder = new SubstringFinder();
 
-    @ParameterizedTest(name = "Text {0} SearchPhrase {1} ")
+    @ParameterizedTest(name = "isSubstring(\"{0}\", \"{1}\") == {2}")
     @MethodSource("provideStrings")
-    public void test(String text, String searchPhrase, int index) {
-        assertEquals(index, substringFinder.find(text, searchPhrase));
+    public void test(String text, String searchPhrase, boolean contains) {
+        assertEquals(contains, substringFinder.isSubstring(text, searchPhrase));
     }
 
     private static Stream<Arguments> provideStrings() {
         return Stream.of(
-                Arguments.of("qwertyuiop", "*", 0),
-                Arguments.of("qwertyuiop", "rty", 3),
-                Arguments.of("qwertyuiop", "rt*", 3),
-                Arguments.of("qwertyuiop", "*ty", 4),
-                Arguments.of("qwertyuiop", "**t*y", 4),
-                Arguments.of("qwertyuiop", "**t*p", 4),
-                Arguments.of("qwertyuiop", "\\*ty", -1),
-                Arguments.of("qwe*tyuiop", "\\*ty", 3),
-                Arguments.of("qwerty", "qwertyuiop", -1)
+                Arguments.of("a*bcd\\", "a*d\\", true),
+                Arguments.of("a*bcd\\", "cd\\", true),
+                Arguments.of("", "*", true),
+                Arguments.of("", "", true),
+                Arguments.of("\\", "\\", true),
+                Arguments.of("\\\\", "\\", true),
+                Arguments.of(" ", "", true),
+                Arguments.of("dynamic", "", true),
+                Arguments.of("\\\\\\", "\\", true),
+                Arguments.of("it is very long long long long long long test", "it * test", true),
+                Arguments.of("//*/\\", "/*/\\", true),
+                Arguments.of("a * rr !", "a \\** !", true),
+                Arguments.of("zAq!@wSXxq\\", "q!*q\\", true),
+                Arguments.of("q!@wSXxq\\", "q!*q\\", true),
+                Arguments.of("a*bcd\\\\", "a*d\\", true),
+                Arguments.of("a*bcd\\\\", "cd\\\\", true),
+                Arguments.of("", "*", true),
+                Arguments.of("", "*t**e**s*****t*", false),
+                Arguments.of("\\\\", "\\\\", true),
+                Arguments.of("\\\\\\\\", "\\\\", true),
+
+                Arguments.of("qwertyuiop", "*", true),
+                Arguments.of("qwertyuiop", "rty", true),
+                Arguments.of("qwertyuiop", "rt*", true),
+                Arguments.of("qwertyuiop", "*ty", true),
+                Arguments.of("qwertyuiop", "**t*y", true),
+                Arguments.of("qwertyuiop", "qwer**ty*ui**o*p", true),
+                Arguments.of("qwertyuiop", "**t*p", true),
+                Arguments.of("qwertyuiop", "\\*ty", false),
+                Arguments.of("qwe*tyuiop", "\\*ty", true),
+                Arguments.of("qwe*tyuiop", "nana", false),
+                Arguments.of("qwerty", "qwertyuiop", false)
         );
     }
 }
